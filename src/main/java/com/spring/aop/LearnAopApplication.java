138b0cd8bd9df26776d20e5864ab1ad636980b34
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-02 06:40:49 +0700

    first

---
+++ src/main/java/com/spring/aop/LearnAopApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAopApplication.class, args);
+	}
+
+}
