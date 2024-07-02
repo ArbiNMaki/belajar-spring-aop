d72c2f2fcfd73e09c66883a289ad4557f983524a
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-02 07:39:19 +0700

    Pointcut

--- src/main/java/com/spring/aop/aspect/LogAspect.java
+++ src/main/java/com/spring/aop/aspect/LogAspect.java
@@ -1,9 +1,14 @@
 package com.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LogAspect {
+
+    @Pointcut("")
+    public void helloServiceMethod() {
+    }
 }
